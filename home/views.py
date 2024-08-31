95f19619a0c0491a85674f8820f3ef40467b2a93
Alexander Gutierrez <johnalexander.gutierrezgaviria@outlook.com>
2024-08-31 14:00:57 -0400

    Implemented user signup functionality, including form, view, template, and URL routing for user registration.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
 # from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from datetime import datetime
 
 # Create your views here.
@@ -19,3 +23,22 @@ class LoginInterfaceView(LoginView):
 
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/signup.html'
+    success_url = '/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes:index')
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        """
+        if the form is valid, authenticate the user and
+        redirect to the supplied URL.
+        """
+        self.object = form.save()
+        login(self.request, self.object)
+        return HttpResponseRedirect(self.get_success_url())
